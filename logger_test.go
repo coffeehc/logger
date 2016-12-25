1bfb928bb35c7981cea7245b9c1f14f3a5f305d9
黄川 <coffeehc@gmail.com>
2016-12-25 17:28:12 +0800

    add golint check

--- logger_test.go
+++ logger_test.go
@@ -1,4 +1,3 @@
-// logger_test.go
 package logger_test
 
 import (
@@ -10,13 +9,13 @@ import (
 
 func TestBaseLog(t *testing.T) {
 	//addStdOutFilter(LOGGER_LEVEL_DEBUG, "/", "", "%T-%L-%C-%M")
-	////	addFileFilterForTime(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log", time.Second*10, 2)
-	////	addFileFilterForDefualt(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log")
-	////	addFileFilterForSize(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log", 3*1024, 2)
+	//	addFileFilterForTime(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log", time.Second*10, 2)
+	//	addFileFilterForDefualt(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log")
+	//	addFileFilterForSize(LOGGER_LEVEL_DEBUG, "/", "/Users/coffee/logs/testLog.log", 3*1024, 2)
 	//for a := 0; a < 10000; a++ {
 	//	Debug("test,time is %s ====%d", time.Now(), a)
 	//}
-	log := logger.CreatLoggerAdapter(logger.LOGGER_LEVEL_ERROR, "", "", os.Stderr)
+	log := logger.CreatLoggerAdapter(logger.LoggerLevelError, "", "", os.Stderr)
 	log.Printf("错误:%s", time.Now())
 	//time.Sleep(time.Second)
 }
